51d5d0faeffca0813127afc5509ec053ec23125e
Matti Haavikko <haavikko@gmail.com>
2021-01-07 18:13:06 +0200

    AboutQuery

--- src/templates/myotatunto-page.js
+++ src/templates/myotatunto-page.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 export const pageQuery = graphql`
-  query AboutQuery($id: String!){
+  query MyotatuntoQuery($id: String!){
 		markdownRemark(id: { eq: $id }) {
       id
 			html
