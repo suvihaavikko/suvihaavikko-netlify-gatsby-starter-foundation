59da1bb314aa77ca08c69397e926d542d1018b90
Matti Haavikko <haavikko@gmail.com>
2021-01-07 18:14:02 +0200

    translations

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,11 +5,11 @@ import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 const MenuItems = [
   {
     path: "/",
-    title: "Home"
+    title: "Alkuun"
   },
   {
-    path: "/about",
-    title: "About"
+    path: "/myotatunto",
+    title: "Myötätuntokeskeinen terapia"
   },
   {
     path: "/blog",
