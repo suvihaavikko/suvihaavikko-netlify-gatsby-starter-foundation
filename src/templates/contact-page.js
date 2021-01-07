0675460badd04e4417f7db595db5579531adeef3
Matti Haavikko <haavikko@gmail.com>
2021-01-07 18:13:50 +0200

    contact page translations

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -39,19 +39,19 @@ const Contact = ({data}) => {
         <form className="contact-form" action="/thanks" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
           <input type="hidden" name="form-name" value="contact" />
           <p>
-            <label>Name<input type="text" name="name" /></label>   
+            <label>Nimi<input type="text" name="name" /></label>
           </p>
           <p>
-            <label>Email<input type="email" name="email" /></label>
+            <label>Sähköpostiosoite<input type="email" name="email" /></label>
           </p>
           <p>
-            <label>Subject<input type="text" name="subject" /></label>   
+            <label>Viestin aihe<input type="text" name="subject" /></label>
           </p>
           <p>
-            <label>Message<textarea name="message"></textarea></label>
+            <label>Viesti<textarea name="message"></textarea></label>
           </p>
           <p className="text-align-right">
-            <button className="button" type="submit">Send Message <span className="icon -right"><RiSendPlane2Line/></span></button>
+            <button className="button" type="submit">Lähetä viesti <span className="icon -right"><RiSendPlane2Line/></span></button>
           </p>
         </form>
       </div>
